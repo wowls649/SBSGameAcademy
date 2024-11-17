03c3d21fb4f52f5451a9e3320156270370fbdca5
wowls649 <wowls2007@gmail.com>
2024-11-17 21:00:04 +0900

    (2024.11.15) 오버플로우 & 언더플로우

--- Program/Source.c
+++ Program/Source.c
@@ -2,88 +2,79 @@
 
 void main()
 {
-#pragma region 비트
-	// 데이터를 나타내는 최소 단위이며, 0 또는 1의 
-	// 조합으로 논리 계산을 수행하는 단위입나다.
-
-	// char alphabet = 15; // [0][0][0][0][1][1][1][1]
-
-#pragma region (10)진수를 (2)진수로 변환하는 과정
-	// 10진수를 1이 될 때까지 계속 2로 나누어 준 다음 
-	// 나눈 위치의 나머지 값을 아래에서 위로 순서대로 
-	// 정렬합니다.
-
-#pragma endregion
-
-#pragma region (2)진수를 (10)진수로 변환하는 과정
-	// 1 byte에 2진수로 저장된 값을 2의 제곱으로
-	// 표현하며, 각각의 비트에 1이 있다면 1과 2의
-	// 제곱의 위치를 계산한 다음 각각의 비트를 모두 
-	// 더하여 10 진수로 나타냅니다.
-
-#pragma endregion
-
-
-	//메모리는 비트 단위로 데이터를 저장할 수 있으며,
-	// 1개의 비트에는 0 또는 1의 값만 저장할 수 있습니다.
-#pragma endregion
-
-#pragma region 비트 연산자
-	// 비트 단위로 논리 연산을 수행하기 위해
-	// 사용하는 연산자입니다.
-
-#pragma region AND 연산자
-	// 두 개의 피연산자가 모두 1 이면 1을 반환
-	//하는 연산자입니다.
-
-
-	// char x1 = 9;  // [0][0][0][0] [1][0][0][1]
-	// char y1 = 13; // [0][0][0][0] [1][1][0][1]
-    //               // -------------------------
-	//               // [0][0][0][0] [1][0][0][1]
+#pragma region 오버플로우
+	// // 특정한 자료형이 표현할 수 있는 최댓값의 범위를
+	// // 넘어서 연산을 수행하는 과정입니다.
 	// 
-	// printf("x1과 y1를 AND 연산한 결과 : %d\n", x1 & y1);
-
-#pragma endregion
-
-#pragma region  OR 연산자
-	// // 두개의 피연산자 중에 하나라도 1이 있디면
-	// // 1을 반복하는 연산자입니다.
-	//
-	// int x2 = 5;  // [0][0][0][0][0][1][0][1]
-	// int y2 = 17; // [0][0][0][1][0][0][0][1]
-	//             // ------------------------
-	//             // [0][0][0][1][0][1][0][1]
-	//
-	// printf("x2와 y2를 OR 연산한 결과 : %d\n", x2 | y2);
-
-#pragma endregion
-
-#pragma region XOR 연산자
-	// 두 개의 피연산자가 서로 같으면 0을 반환하고,
-	// 서로 다르면 1을 변환하는 연산자입나다.
-
-	//nt x3 = 14; // [0][0][0][0][1][1][1][0]
-	//nt y3 = 7;  // [0][0][0][0][0][1][1][1]
-	//	         // ------------------------
-	//            // [0][0][0][0][1][0][0][1] = 9
-	//
-	//rintf("x3와 y3의 XOR 연산자 결과 : %d\n,", x3 ^ y3);
-
-#pragma endregion
-
-#pragma region NOT 연산자
-	// 비트를 반전시키는 연산자입니다.
-
-	// char flag = 16; // [0][0][0][1][0][0][0][0]
-	//                 // ------------------------
-	//                 // [1][1][1][0][1][1][1][1]
+	// char character = 129; // <-   -128
 	// 
-	// printf("flag 변수를 NOT 연산자 결과 : %d\n", ~flag);
-	
+	// printf("character 변수의 값 : %d\n", character);
+	// 
+	// // 오버플로우는 부호 없는 자료형에서도 똑같이 발생하며,
+	// // 실수일 떄, 오버플로우가 발생하면 infinity라는 값을 출력합니다.
+#pragma endregion
+
+#pragma region 언더플로우
+	// 특정한 자료형이 표현할 수 있는 최솟값의 범위
+	// 를 넘어서 연산을 수행하는 과정입니다.
+
+	// char alphabet = -130;
+	 
+	// printf("alphabet 변수의 값 : %d\n", alphabet);
+
+	// 언더플로우는 부호 없는 자료형에서도 똑같이 발생
+	// 하며, 실수일 대 언더플로우가 발생하면 0이라는 값이 출력됩니다.
 
 #pragma endregion
 
+#pragma region 시프트 연산자
+	// // 비트의 위치를 오른쪽 또는 왼쪽으로 특정한 수만큼
+	// // 이동시키는 연산자입니다.
+	// 
+	// int x = 10;
+	// int y = 12;
+	// 
+	// printf("x의 값을 2번 왼쪽으로 이동한 결과 : %d\n", x << 2);
+	// printf("y의 값을 2번 왼쪽으로 이동한 결과 : %d\n", y >> 2);
+
+#pragma endregion
+
+#pragma region 산술 연산자
+
+	// int data = 10;
+	// const int constant = 10;
+	// 
+	// int result1 = data + data;
+	// int result2 = 15 - data;
+	// int result3 = constant * 5;
+	// int result4 = 15 / 15;
+	// int result5 = constant % constant;
+	// 
+	// printf("result1 변수의 값 : %d\n", result1);
+	// printf("result2 변수의 값 : %d\n", result2);
+	// printf("result3 변수의 값 : %d\n", result3);
+	// printf("result4 변수의 값 : %d\n", result4);
+	// printf("result5 변수의 값 : %d\n", result5);
+	// 
+	// // int result1 <- 변수 + 변수
+	// // int result2 <- 리터널 상수 - 변수
+	// // int result3 <- 심볼릭 상수 * 리터널 상수
+	// // int result4 <- 리터럴 상수 / 리터널 상수
+	// // int result5 <- 심볼릭 살수 % 심볼릭 상수
+
+
+#pragma endregion
+
+#pragma region 부호 없는 자료형
+
+	unsigned short mineral = 65535;
+
+	unsigned int gas = -1;
+
+	printf("mineral 변수의 값 : % d\n", mineral);
+
+	printf("gas 변수의 값 : %u\n", gas);
+	printf("gas 변수의 값 : %d\n", gas);
 
 #pragma endregion
 
